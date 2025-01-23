9d06f11f7e78542cc5b3f8dfd84d52c528edbae4
Gunal-k <gunalk.0007@gmail.com>
2025-01-24 00:11:02 +0530

    commit 20

--- app/main.py
+++ app/main.py
@@ -39,7 +39,7 @@ def completer(text, state):
             sys.stdout.flush()
         last_tab_pressed["count"] = 0
         return None
-    return options[state] if state < len(options2) else None
+    return options[state] if state < len(options) else None
 
 def main():
     # Uncomment this block to pass the first stage
