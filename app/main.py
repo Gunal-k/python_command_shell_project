4ae757674fe33d612867386d7f3d90bca6f15323
Gunal-k <gunalk.0007@gmail.com>
2025-01-23 16:31:05 +0530

    commit 3

--- app/main.py
+++ app/main.py
@@ -3,14 +3,15 @@ import sys
 
 def main():
     # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
+    while True:
+        sys.stdout.write("$ ")
 
-    # Wait for user input
-    command = input()
+        # Wait for user input
+        command = input()
 
-    if command.startswith("invalid"):
-        sys.stdout.write(f"{command}: command not found\n")
-        sys.exit()
+        if command.startswith("invalid"):
+            sys.stdout.write(f"{command}: command not found\n")
+            sys.exit()
 
 if __name__ == "__main__":
     main()
