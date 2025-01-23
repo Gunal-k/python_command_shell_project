fd73dac1903c42a2a6c1758e471a7140578138e2
Gunal-k <gunalk.0007@gmail.com>
2025-01-23 17:29:00 +0530

    commit 13

--- app/main.py
+++ app/main.py
@@ -22,7 +22,7 @@ def main():
             if "\\" in value:
                 value = value.replace("\\","")
             elif value.startswith("'") or value.startswith('"'):
-                value = value.replace("'","").replace('"',"")
+                value =" ".join(value.split("\""))
             else:
                 value = " ".join(shlex.split(value))
             sys.stdout.write(f"{value}\n")
